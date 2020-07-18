1c190f76d817e97a7a8c0cac0a6e885f64c4d965
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-18 21:54:37 +0530

    use case 2 completed

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+declare -A coin
 singlet=(H T)
+for key in ${singlet[@]}
+do
+        coin[$key]=0
+done
+
 for((i=0;i<10;i++))
 do
-	x=$((RANDOM%2))
-	echo ${singlet[$x]}
+        x=$((RANDOM%2))
+        coin[${singlet[$x]}]=$((${coin[${singlet[$x]}]}+1))
 done
-echo ${coin[@]}
+
+#since i am running the loop for 10 times so percentage is calculated by val*10
+echo "Percentage of Heads" $((${coin[H]}*10))
+echo "Percentage of Tails" $((${coin[T]}*10))
